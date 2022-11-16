b2ba90ba6748c956272de1663e5e7758d2978b82
haejun1 <a01027833227@gmail.com>
2022-11-16 20:40:51 +0900

    feat  nav추가

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // 로그인 페이지
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/", (req, res) => {
+  res.render("login", {});
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 // 로그인
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const id = req.body.id;
   const pw = req.body.pw;
 });

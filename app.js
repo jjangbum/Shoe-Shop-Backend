392fe9fe75e42a7f577965c2ca10afcff8e801b0
haejun1 <a01027833227@gmail.com>
2022-11-18 14:06:59 +0900

    feat session 추가
    express-session 추가
    express-mysql-session 추가
    session에 DB로 연결된 sessionStore를 객체로 지정

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
-const itemRouter = require('./routes/item');
-const cartRouter = require('./routes/cart');
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const itemRouter = require("./routes/item");
+const cartRouter = require("./routes/cart");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/user', userRouter);
-app.use('/item', itemRouter);
-app.use('/cart', cartRouter);
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.use("/item", itemRouter);
+app.use("/cart", cartRouter);
+
+//session사용
+const options = {
+  host: "localhost",
+  port: 3002,
+  id: "",
+  password: "",
+  database: "",
+};
+
+const sessionStore = new MySQLStore(options);
+
+app.use(
+  session({
+    secret: "0000",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore, //저장소를 DB로 연결된 sessionStore 객체로 지정
+  })
+);
 
 // error page
 app.use((req, res, next) => {
-  res.status(404).send('error');
+  res.status(404).send("error");
 });
 
 app.listen(3002);

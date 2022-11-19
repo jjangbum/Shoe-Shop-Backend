482d36757b95973fdb07ab6c2fe1d79e678557f3
haejun1 <a01027833227@gmail.com>
2022-11-19 11:57:00 +0900

    feat json으로 로그인 정보 저장

--- routes/user.js
+++ routes/user.js
@@ -1,60 +1,83 @@
 const express = require("express");
 const router = express.Router();
 
-const userInfo = {
-  id: "a",
-  password: "a",
-};
-// 로그인 페이지
+const userInfo = [
+  {
+    id: "doraemon49",
+    password: "0000",
+  },
+  {
+    id: "Bokyeom",
+    password: "0000",
+  },
+  {
+    id: "haejun1",
+    password: "0000",
+  },
+];
+
+router.get("/login", (req, res) => {
+  res.json(userInfo);
+});
+
 router.get("/", (req, res) => {
   res.render("login", {});
 });
 
-router.get("/login", (req, res) => {
-  const post = req.body;
-  db.query(
-    "select member.id as id, password, author_id, name from member left join author on member.author_id = author.id where member.id=? and password=?",
-    [post.id, post.password],
-    (err, result) => {
-      if (err) throw err;
-      if (result[0] !== undefined) {
-        req.session.uid = result[0].id;
-        req.session.author_id = result[0].author_id;
-        req.session.isLogined = true;
-        req.session.save(function () {
-          res.redirect("/");
-        });
-      }
-    }
-  );
-});
+// const userInfo = {
+//   id: "a",
+//   password: "a",
+// };
+// // 로그인 페이지
+// router.get("/", (req, res) => {
+//   res.render("login", {});
+// });
 
-// 로그인
-router.post("/login", (req, res) => {
-  if (req.body.id == userInfo.id && req.body.password == userInfo.password) {
-    req.body.user = req.body.id;
-    res.redirect("/user/approve");
-  } else {
-    res.redirect("/user");
-  }
-});
+// router.get("/login", (req, res) => {
+//   const post = req.body;
+//   db.query(
+//     "select member.id as id, password, author_id, name from member left join author on member.author_id = author.id where member.id=? and password=?",
+//     [post.id, post.password],
+//     (err, result) => {
+//       if (err) throw err;
+//       if (result[0] !== undefined) {
+//         req.session.uid = result[0].id;
+//         req.session.author_id = result[0].author_id;
+//         req.session.isLogined = true;
+//         req.session.save(function () {
+//           res.redirect("/");
+//         });
+//       }
+//     }
+//   );
+// });
 
-router.get("/approve", (req, res) => {
-  res.render("index", {
-    user: req.body.user,
-  });
-});
+// // 로그인
+// router.post("/login", (req, res) => {
+//   if (req.body.id == userInfo.id && req.body.password == userInfo.password) {
+//     req.body.user = req.body.id;
+//     res.redirect("/user/approve");
+//   } else {
+//     res.redirect("/user");
+//   }
+// });
 
-router.get("/logout", (req, res) => {
-  delete req.session.uid;
-  delete req.session.isLogined;
-  delete req.session.author_id;
+// router.get("/approve", (req, res) => {
+//   res.render("index", {
+//     user: req.body.user,
+//   });
+// });
 
-  req.session.save(function () {
-    res.render("index", {
-      logout: "로그아웃 되었습니다.",
-    });
-  });
-});
+// router.get("/logout", (req, res) => {
+//   delete req.session.uid;
+//   delete req.session.isLogined;
+//   delete req.session.author_id;
+
+//   req.session.save(function () {
+//     res.render("index", {
+//       logout: "로그아웃 되었습니다.",
+//     });
+//   });
+// });
 
 module.exports = router;

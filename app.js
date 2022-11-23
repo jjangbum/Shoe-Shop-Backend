734f3c82a044558d15844c1b88569af2d636e76f
k-kbk <kkbk-@naver.com>
2022-11-23 18:33:04 +0900

    점검

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
-const userRouter = require("./routes/user");
-const itemRouter = require("./routes/item");
-const cartRouter = require("./routes/cart");
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const itemRouter = require('./routes/item');
+const cartRouter = require('./routes/cart');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/user", userRouter);
-app.use("/item", itemRouter);
-app.use("/cart", cartRouter);
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use('/item', itemRouter);
+app.use('/cart', cartRouter);
 
 //login
 app.use(
   cors({
-    //cors : 도메인과 포트에서 리소스를 요청하고 받는
-    origin: true,
-    credentials: true,
+    origin: '*',
   })
 );
 app.use(cookieParser()); //요청된 쿠키를 쉽게 추출할 수 있도록 함
 app.use(
   session({
-    key: "loginData",
-    secret: "testSecret", //위 2개는 env파일?
+    key: 'userInfo',
+    secret: 'testSecret', //위 2개는 env파일?
     resave: false, //요청이 올 때 세션에 수정 사항이 생기지 않더라도 세션을 다시 저장할지 설정
     saveUninitialized: false, //세션에 저장할 내용이 없더라도 처음부터 세션을 생성할지 설정
     cookie: {
@@ -48,7 +46,7 @@ app.use(
 
 // error page
 app.use((req, res, next) => {
-  res.status(404).send("error");
+  res.status(404).send('error');
 });
 
 app.listen(3002);

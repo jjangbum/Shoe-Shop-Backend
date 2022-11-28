84e9cddf9111cb88abb37d1248154335af68e919
k-kbk <kkbk-@naver.com>
2022-11-28 18:34:40 +0900

    feat: 에러 처리

--- app.js
+++ app.js
@@ -40,7 +40,15 @@ app.use('/item', itemRouter);
 app.use('/cart', cartRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('error');
+  res.status(404).send('404');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: 'Server Error',
+    error: err,
+  });
 });
 
 app.listen(3002);
